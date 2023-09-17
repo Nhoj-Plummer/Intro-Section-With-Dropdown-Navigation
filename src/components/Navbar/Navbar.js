0f997f0b180e5e1f31a9332ac907d960ab64ec74
Nhoj <nhojplummer@yahoo.com>
2023-09-16 20:23:24 -0400

    Making progress

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,22 +2,22 @@ import React, { useState } from "react"
 import "./navbar.css"
 import Logo from "../../assets/logo.svg"
 
-// const Menu = () => (
-//   <>
-//     <p>
-//       <a href="#features"> Features</a>
-//     </p>
-//     <p>
-//       <a href="#company">Company</a>
-//     </p>
-//     <p>
-//       <a href="#careers">Careers</a>
-//     </p>
-//     <p>
-//       <a href="#about"> About</a>
-//     </p>
-//   </>
-// )
+const Menu = () => (
+  <>
+    <p>
+      <a href="#features"> Features</a>
+    </p>
+    <p>
+      <a href="#company">Company</a>
+    </p>
+    <p>
+      <a href="#careers">Careers</a>
+    </p>
+    <p>
+      <a href="#about"> About</a>
+    </p>
+  </>
+)
 
 const Navbar = () => {
   // const [toggleMenu, setToggleMenu] = useState(true)
@@ -28,6 +28,15 @@ const Navbar = () => {
         <img src={Logo} alt="Logo" />
       </div>
       {/* Desktop */}
+      <div className="navbar-desktop-container">
+        <div className="navbar-desktop-menu">
+          <Menu />
+        </div>
+        <div className="navbar-desktop-login">
+          <a href="#login">Login</a>
+          <a href="#register">Register</a>
+        </div>
+      </div>
 
       {/* Mobile */}
     </div>
