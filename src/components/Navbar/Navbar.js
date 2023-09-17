70a4d2209fe1eb1000462d10a192c47592ccfe90
Nhoj <nhojplummer@yahoo.com>
2023-09-16 21:14:09 -0400

    Adding mobile menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -46,6 +46,13 @@ const Navbar = () => {
         <div className="navbar-mobile-toggle">
           <img src={Menu1} alt="toggle" />
         </div>
+        <div className="navbar-mobile_menu">
+          <Menu />
+          <div className="navbar-mobile-login">
+            <a href="#login">Login</a>
+            <a href="#register">Register</a>
+          </div>
+        </div>
       </div>
     </div>
   )
