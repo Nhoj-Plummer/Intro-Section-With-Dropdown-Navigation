2b9a4675245019c4ecac4ce727ee5c2fbc0829c9
Nhoj <nhojplummer@yahoo.com>
2023-09-23 12:54:12 -0400

    Image assets have been added

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,22 +1,24 @@
 import React from "react"
 import "./HeroSection.css"
+import {DesktopImg, MobileImg} from "../../assets/assestindex"
 
 const HeroSection = () => {
   return (
     <div className="hero-section_container">
       <div className="hero-section_image">
         <picture>
-
+          <source media="(min-width: 541px)" srcSet={DesktopImg} />
+          <img src={MobileImg} alt="img" />
         </picture>
       </div>
       <div className="hero-section_text">
-        <h1></h1>
-        <p></p>
+        <h1>Make remote work</h1>
+        <p>Get your team in sync, no matter your location. Streamline Processes, create team rituals, and watch productivity soar.</p>
         <button>
           <p>Learn More</p>
         </button>
       </div>
-
+      <div className="hero-section_groups"></div>
     </div>
   )
 }
