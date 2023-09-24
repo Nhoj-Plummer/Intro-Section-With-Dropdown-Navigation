2937b9309dc7b0bb4270d00c14674725e2ef850e
Nhoj <nhojplummer@yahoo.com>
2023-09-23 20:21:47 -0400

    Fixed the styling issue

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -14,8 +14,10 @@ const HeroSection = () => {
   return (
     <div className="hero-section_container section-padding">
       <div className="hero-section_image">
-        <source media="(min-width: 541px)" srcSet={DesktopImg} />
-        <img src={MobileImg} alt="img" />
+        <picture>
+          <source media="(min-width: 541px)" srcSet={DesktopImg} />
+          <img src={MobileImg} alt="img" />
+        </picture>
       </div>
       <div className="hero-section_text">
         <h1>Make remote work</h1>
